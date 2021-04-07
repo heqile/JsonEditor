e1ab7fc158071975610708848d4b437963eb451a
qile <mr.qile@gmail.com>
2021-04-07 13:56:14 +0200

    minimize when click close button

--- JsonEditor/Editor.cs
+++ JsonEditor/Editor.cs
@@ -119,7 +119,22 @@ namespace JsonEditor
 
         private void Notifier_DoubleClick(object sender, EventArgs e)
         {
+            this.Show();
             WindowState = FormWindowState.Normal;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.WindowsShutDown
+                || e.CloseReason == CloseReason.ApplicationExitCall
+                || e.CloseReason == CloseReason.TaskManagerClosing)
+            {
+                return;
+            }
+            e.Cancel = true;
+            //assuming you want the close-button to only hide the form, 
+            //and are overriding the form's OnFormClosing method:
+            this.Hide();
         }
     }
 }
