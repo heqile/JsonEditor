b4016cfdeb68d0d3d577849169af20f4def05b79
qile <mr.qile@gmail.com>
2021-04-09 21:39:24 +0200

    refactor GetFormattedJson

--- JsonEditor/EditorModel.cs
+++ JsonEditor/EditorModel.cs
@@ -41,6 +41,29 @@ namespace JsonEditor
         public string GetFormattedJson()
         {
             m_content = m_content.Replace("\n", "\r\n");  // replace new line
+            string formattedJson = GetFormattedJsonIfSameAsPrevious();
+            if (!string.IsNullOrEmpty(formattedJson))
+            {
+                return formattedJson;
+            }
+
+            m_jsonContent = new JsonContent(m_content);
+            if (!m_jsonContent.IsValidJson)
+            {
+                var exception = new ArgumentOutOfRangeException(m_jsonContent.ErrorMessage);
+                throw (exception);
+            }
+
+            formattedJson = GetFormattedJsonIfSameAsPrevious();
+            if (!string.IsNullOrEmpty(formattedJson))
+            {
+                return formattedJson;
+            }
+            return m_jsonContent.GetIndentedJson();
+        }
+
+        private string GetFormattedJsonIfSameAsPrevious()
+        {
             if (m_content == m_jsonContent.GetCompactJson())
             {
                 return m_jsonContent.GetIndentedJson();
@@ -49,27 +72,7 @@ namespace JsonEditor
             {
                 return m_jsonContent.GetCompactJson();
             }
-            else
-            {
-                m_jsonContent = new JsonContent(m_content);
-                if (m_jsonContent.IsValidJson)
-                {
-                    if (m_content == m_jsonContent.GetCompactJson())
-                    {
-                        return m_jsonContent.GetIndentedJson();
-                    }
-                    else if (m_content == m_jsonContent.GetIndentedJson())
-                    {
-                        return m_jsonContent.GetCompactJson();
-                    }
-                    else
-                    {
-                        return m_jsonContent.GetIndentedJson();
-                    }
-                }
-                var exception = new ArgumentOutOfRangeException(m_jsonContent.ErrorMessage);
-                throw (exception);
-            }
+            return string.Empty;
         }
 
         public string GetIndentedJson()
