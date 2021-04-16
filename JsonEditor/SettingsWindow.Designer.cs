2c1b25cb7eb495358164c9a938f60b864f0a74cb
qile <mr.qile@gmail.com>
2021-04-16 23:56:26 +0200

    update settings window

--- JsonEditor/SettingsWindow.Designer.cs
+++ JsonEditor/SettingsWindow.Designer.cs
@@ -40,7 +40,8 @@ namespace JsonEditor
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(276, 98);
+            this.SaveButton.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SaveButton.Location = new System.Drawing.Point(248, 86);
             this.SaveButton.Name = "SaveButton";
             this.SaveButton.Size = new System.Drawing.Size(91, 27);
             this.SaveButton.TabIndex = 0;
@@ -50,7 +51,8 @@ namespace JsonEditor
             // 
             // CancelButton
             // 
-            this.CancelButton.Location = new System.Drawing.Point(388, 98);
+            this.CancelButton.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.CancelButton.Location = new System.Drawing.Point(360, 86);
             this.CancelButton.Name = "CancelButton";
             this.CancelButton.Size = new System.Drawing.Size(91, 27);
             this.CancelButton.TabIndex = 0;
@@ -61,37 +63,43 @@ namespace JsonEditor
             // ShortkeyBindingLabel
             // 
             this.ShortkeyBindingLabel.AutoSize = true;
-            this.ShortkeyBindingLabel.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.ShortkeyBindingLabel.Location = new System.Drawing.Point(40, 43);
+            this.ShortkeyBindingLabel.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ShortkeyBindingLabel.Location = new System.Drawing.Point(25, 29);
             this.ShortkeyBindingLabel.Name = "ShortkeyBindingLabel";
-            this.ShortkeyBindingLabel.Size = new System.Drawing.Size(179, 20);
+            this.ShortkeyBindingLabel.Size = new System.Drawing.Size(159, 28);
             this.ShortkeyBindingLabel.TabIndex = 1;
-            this.ShortkeyBindingLabel.Text = "Shortcut binding:";
+            this.ShortkeyBindingLabel.Text = "HotKey binding :";
+            this.ShortkeyBindingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ModifierKeyBindingTextBox
             // 
-            this.ModifierKeyBindingTextBox.Location = new System.Drawing.Point(232, 41);
+            this.ModifierKeyBindingTextBox.Font = new System.Drawing.Font("Leelawadee UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ModifierKeyBindingTextBox.Location = new System.Drawing.Point(204, 29);
             this.ModifierKeyBindingTextBox.Name = "ModifierKeyBindingTextBox";
             this.ModifierKeyBindingTextBox.ReadOnly = true;
-            this.ModifierKeyBindingTextBox.Size = new System.Drawing.Size(126, 25);
+            this.ModifierKeyBindingTextBox.Size = new System.Drawing.Size(126, 30);
             this.ModifierKeyBindingTextBox.TabIndex = 2;
+            this.ModifierKeyBindingTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.ModifierKeyBindingTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ModifierKeyBindingTextBox_KeyDown);
             // 
             // MainKeyBindingTextBox
             // 
-            this.MainKeyBindingTextBox.Location = new System.Drawing.Point(409, 41);
+            this.MainKeyBindingTextBox.Font = new System.Drawing.Font("Leelawadee UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MainKeyBindingTextBox.Location = new System.Drawing.Point(381, 29);
             this.MainKeyBindingTextBox.Name = "MainKeyBindingTextBox";
             this.MainKeyBindingTextBox.ReadOnly = true;
-            this.MainKeyBindingTextBox.Size = new System.Drawing.Size(79, 25);
+            this.MainKeyBindingTextBox.Size = new System.Drawing.Size(79, 30);
             this.MainKeyBindingTextBox.TabIndex = 3;
+            this.MainKeyBindingTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.MainKeyBindingTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainKeyBindingTextBox_KeyDown);
             // 
             // ShortcutBindingPlusLabel
             // 
             this.ShortcutBindingPlusLabel.AutoSize = true;
-            this.ShortcutBindingPlusLabel.Location = new System.Drawing.Point(373, 44);
+            this.ShortcutBindingPlusLabel.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ShortcutBindingPlusLabel.Location = new System.Drawing.Point(344, 30);
             this.ShortcutBindingPlusLabel.Name = "ShortcutBindingPlusLabel";
-            this.ShortcutBindingPlusLabel.Size = new System.Drawing.Size(15, 15);
+            this.ShortcutBindingPlusLabel.Size = new System.Drawing.Size(26, 28);
             this.ShortcutBindingPlusLabel.TabIndex = 4;
             this.ShortcutBindingPlusLabel.Text = "+";
             // 
@@ -99,18 +107,20 @@ namespace JsonEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 143);
+            this.ClientSize = new System.Drawing.Size(488, 131);
             this.Controls.Add(this.ShortcutBindingPlusLabel);
             this.Controls.Add(this.MainKeyBindingTextBox);
             this.Controls.Add(this.ModifierKeyBindingTextBox);
             this.Controls.Add(this.ShortkeyBindingLabel);
             this.Controls.Add(this.CancelButton);
             this.Controls.Add(this.SaveButton);
+            this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SettingsWindow";
             this.ShowInTaskbar = false;
             this.Text = "Settings";
+            this.TopMost = true;
             this.Load += new System.EventHandler(this.SettingsWindow_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
