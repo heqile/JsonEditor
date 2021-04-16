f2e0e0bfba4311b6cca34a98b62800339cc67047
qile <mr.qile@gmail.com>
2021-04-16 22:18:49 +0200

    make settings TextBox readonly

--- JsonEditor/SettingsWindow.Designer.cs
+++ JsonEditor/SettingsWindow.Designer.cs
@@ -65,7 +65,7 @@ namespace JsonEditor
             this.ShortkeyBindingLabel.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.ShortkeyBindingLabel.Location = new System.Drawing.Point(40, 43);
             this.ShortkeyBindingLabel.Name = "ShortkeyBindingLabel";
-            this.ShortkeyBindingLabel.Size = new System.Drawing.Size(144, 16);
+            this.ShortkeyBindingLabel.Size = new System.Drawing.Size(179, 20);
             this.ShortkeyBindingLabel.TabIndex = 1;
             this.ShortkeyBindingLabel.Text = "Shortcut binding:";
             // 
@@ -73,6 +73,7 @@ namespace JsonEditor
             // 
             this.ModifierKeyBindingTextBox.Location = new System.Drawing.Point(232, 41);
             this.ModifierKeyBindingTextBox.Name = "ModifierKeyBindingTextBox";
+            this.ModifierKeyBindingTextBox.ReadOnly = true;
             this.ModifierKeyBindingTextBox.Size = new System.Drawing.Size(126, 25);
             this.ModifierKeyBindingTextBox.TabIndex = 2;
             this.ModifierKeyBindingTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ModifierKeyBindingTextBox_KeyDown);
@@ -81,6 +82,7 @@ namespace JsonEditor
             // 
             this.MainKeyBindingTextBox.Location = new System.Drawing.Point(409, 41);
             this.MainKeyBindingTextBox.Name = "MainKeyBindingTextBox";
+            this.MainKeyBindingTextBox.ReadOnly = true;
             this.MainKeyBindingTextBox.Size = new System.Drawing.Size(79, 25);
             this.MainKeyBindingTextBox.TabIndex = 3;
             this.MainKeyBindingTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainKeyBindingTextBox_KeyDown);
